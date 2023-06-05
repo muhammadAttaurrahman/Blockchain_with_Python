b0dad170f641966d685f99ff7e7bb76e7d7bfcaf
Muhammad Atta ur Rahman <53517520+muhammadAttaurrahman@users.noreply.github.com>
2023-06-05 06:57:36 -0700

    Update p4readANdWriteContract.py

--- p4readANdWriteContract.py
+++ p4readANdWriteContract.py
@@ -2,7 +2,7 @@ import json
 from web3 import Web3, contract 
 
 #In this file we are gonna read and write A smart contract  
-
+#some changes are made to not get compromised
 ganache_url = "http://127.0.0.1:7545"
 web3_var = Web3 (Web3.HTTPProvider(ganache_url))
 print(web3_var.isConnected())
@@ -10,8 +10,8 @@ print(web3_var.eth.block_number)
 
 web3_var.eth.defaultAccount = web3_var.eth.accounts[0] #it means that this contract is gonna apply for this account
 
-abi = json.loads('[{"inputs":[],"stateMutability":"nonpayable","type":"constructor"},{"inputs":[],"name":"getGreetings","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"greetings","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"string","name":"_greeting","type":"string"}],"name":"setGreetings","outputs":[],"stateMutability":"nonpayable","type":"function"}]')
-address = web3_var.toChecksumAddress( "0x3dDE37A4F0575c0eF77dcEc73BB4CA3532EAC8d5")
+abi = json.loads('[{"inputs":[],"stateMutability":"nonpayable","type":"constructor"},{"inputs":[],"name":"getGreetings","outputs":[{"internalType":"string","name":"",":"string"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"greetings","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"string","name":"_greeting","type":"string"}],"name":"setGreetings","outputs":[],"stateMutability":"nonpayable","type":"function"}]')
+address = web3_var.toChecksumAddress( "0x3dDE37A4F0575c0eF77dcEc73BB4CA3532E")
 
 contract =  web3_var.eth.contract(address=address , abi = abi)
 
